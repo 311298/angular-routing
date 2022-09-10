001aa7d5d13755b512674ce2f8508be3484277ec
311298 <kashyapanurag109@gmail.com>
2022-09-11 01:03:16 +0530

    basic operation of angular router and use of router-outlet directive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'department', component: DepartmentListComponent },
+  // what does thing path means if we write link manually and localhost:4200/department -> it will lead to DepartmentListComponent and same goes for EmployeeListComponent
+  { path: 'employee', component: EmployeeListComponent },
+  // each JS object coresponds to component and its path
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents = [
+  DepartmentListComponent,
+  EmployeeListComponent,
+];
+// in order to avoid the same imports inside the app.module and app-routing.module we will create a new variable and export it app.module
+// any time we add new component we only need to add it inside the app-routing.module.ts file not in app.module.ts file
