13564ed86a994f4d361753b855465ab8bd5d7624
311298 <kashyapanurag109@gmail.com>
2022-09-11 11:07:32 +0530

    wildcard routes and redirecting routes to different components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  // { path: '', component: DepartmentListComponent },
+  { path: '', redirectTo: '/department', pathMatch: 'full' },
+  // pathMacth has 2 value 'prefix' -> which add the /department to anything and 'full' path is exactly same
   { path: 'department', component: DepartmentListComponent },
   // what does thing path means if we write link manually and localhost:4200/department -> it will lead to DepartmentListComponent and same goes for EmployeeListComponent
   { path: 'employee', component: EmployeeListComponent },
   // each JS object coresponds to component and its path
+  { path: '**', component: PageNotFoundComponent },
+  //for page not found, and should always be last in JS object
 ];
 
 @NgModule({
@@ -18,6 +24,7 @@ export class AppRoutingModule {}
 export const routingComponents = [
   DepartmentListComponent,
   EmployeeListComponent,
+  PageNotFoundComponent,
 ];
 // in order to avoid the same imports inside the app.module and app-routing.module we will create a new variable and export it app.module
 // any time we add new component we only need to add it inside the app-routing.module.ts file not in app.module.ts file
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, routingComponents],
+  declarations: [AppComponent, routingComponents, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
